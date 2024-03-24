fa5251d821501e6150e568dd11f5c168417481bd
pavlov <pavlov.web@mail.ru>
2024-03-24 19:09:48 +0400

    realize function get data IP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("qqweqewqeqwe");
+const ipInput = document.querySelector('.search-bar__input')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', getData)
+ipInput.addEventListener('keydown', handleKey)
+
+function getData() {
+    // checking data
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_vebQkfyAhmQfwXZul55Fj1qus9HgU&ipAddress=${ipInput.value}`)
+    .then(response => response.json())
+    .then(console.log)
+}
+
+function handleKey(e) {
+    if (e.key === 'Enter') {
+        getData()
+    }
+}
