87ba8d3c2711763a44a1a57c6bb3475e899b8201
pavlov <pavlov.web@mail.ru>
2024-03-25 20:24:05 +0400

    get Data and Print to DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {validateIp} from './helpers'
 const ipInput = document.querySelector('.search-bar__input')
 const btn = document.querySelector('button')
 
+const ipInfo = document.querySelector('#ip')
+const locationInfo = document.querySelector('#location')
+const timeZone = document.querySelector('#timezone')
+const IspInfo = document.querySelector('#isp')
+
 btn.addEventListener('click', getData)
 ipInput.addEventListener('keydown', handleKey)
 
@@ -10,7 +15,7 @@ function getData() {
     if (validateIp(ipInput.value)) {
         fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_vebQkfyAhmQfwXZul55Fj1qus9HgU&ipAddress=${ipInput.value}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(setInfo)
     }
 }
 
@@ -18,4 +23,12 @@ function handleKey(e) {
     if (e.key === 'Enter') {
         getData()
     }
+}
+
+function setInfo(mapData) {
+    console.log(mapData);
+    ipInfo.innerText = mapData.ip
+    locationInfo.innerText = mapData.location.country + " " + mapData.location.region
+    timeZone.innerText = mapData.location.timezone
+    IspInfo.innerText = mapData.isp
 }
